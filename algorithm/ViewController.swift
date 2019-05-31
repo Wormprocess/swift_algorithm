8edfee290599164eabedba26497408843ec026bc
Wormprocess <951472397@qq.com>
2019-05-31 17:34:48 +0800

    tijaio

--- algorithm/ViewController.swift
+++ algorithm/ViewController.swift
@@ -79,9 +79,11 @@ class ViewController: UIViewController {
 //        self.insertSort();
 //        self.hillSort();
 //        self.hillSort2();
-        DispatchQueue.global(qos: .default).async {
-            self.fastSort(a: &self.data, left: 0, right: self.data.count - 1);
-        }
+//        DispatchQueue.global(qos: .default).async {
+//            self.fastSort(a: &self.data, left: 0, right: self.data.count - 1);
+//        }
+        
+        self.heapSort(sortArray: self.data);
     }
     
     //冒泡排序
@@ -366,6 +368,11 @@ class ViewController: UIViewController {
     
     
     // 堆排序
+    /*
+     1.将初始待排序关键字序列(R1,R2….Rn)构建成大顶堆，此堆为初始的无序区；
+     2.将堆顶元素R[1]与最后一个元素R[n]交换，此时得到新的无序区(R1,R2,……Rn-1)和新的有序区(Rn),且满足R[1,2…n-1]<=R[n]；
+     3.由于交换后新的堆顶R[1]可能违反堆的性质，因此需要对当前无序区(R1,R2,……Rn-1)调整为新堆，然后再次将R[1]与无序区最后一个元素交换，得到新的无序区(R1,R2….Rn-2)和新的有序区(Rn-1,Rn)。不断重复此过程直到有序区的元素个数为n-1，则整个排序过程完成。
+     */
     var len :Int = 0
     
     func heapSort(sortArray:[NSInteger]) -> Void {
